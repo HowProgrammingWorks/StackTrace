0d0d5766893679f2a2cf4af0eb6eae99b93444db
tshemsedinov <timur.shemsedinov@gmail.com>
2019-03-17 12:03:31 +0200

    Add example with domain

---
+++ JavaScript/5-domain.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const domain = require('domain');
+
+const d = domain.create();
+
+const f1 = callback => {
+  setTimeout(() => {
+    callback(new Error('Where am I?'));
+  }, 0);
+};
+
+const f2 = callback => f1(callback);
+
+const f3 = callback => f2(callback);
+
+d.on('error', e => {
+  console.log('Catch', e);
+});
+
+d.run(() => {
+  setTimeout(() => {
+    f3(err => {
+      setTimeout(() => {
+        throw err;
+      }, 0);
+    });
+  }, 0);
+});
