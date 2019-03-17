78fbf3fba2f58c86e6fa21c95dd08510f2de5968
tshemsedinov <timur.shemsedinov@gmail.com>
2019-03-17 11:40:15 +0200

    Add example with callback contract

---
+++ JavaScript/4-callback.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const f1 = callback => {
+  setTimeout(() => {
+    callback(new Error('Where am I?'));
+  }, 0);
+};
+
+const f2 = callback => {
+  f1(callback);
+};
+
+const f3 = callback => {
+  f2(callback);
+};
+
+f3(err => {
+  console.log('Catch', err);
+});
