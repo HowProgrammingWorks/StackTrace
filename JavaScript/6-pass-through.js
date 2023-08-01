b22d18968a740fa6ca8342bc9e2934b6dba93838
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-08-01 15:08:58 +0300

    Add new code examples

---
+++ JavaScript/6-pass-through.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const timers = require('node:timers/promises');
+
+const length = async (line) => {
+  await timers.setTimeout(100);
+  if (line.trim() === '') throw new Error('Just spaces');
+  return line.length;
+};
+
+const checkType = async (line) => {
+  await timers.setTimeout(100);
+  if (line === undefined) throw new Error('Argument is undefined');
+  if (typeof line !== 'string') throw new Error('Argument is not a string');
+  if (line === '') throw new Error('Argument is empty string');
+};
+
+const getLength = async (line) => {
+  await timers.setTimeout(100);
+  await checkType(line);
+  return length(line); // no await in this line
+};
+
+const main = async () => {
+  const line = 'Hello World!';
+  const length = await getLength(line);
+  console.log({ line, length });
+};
+
+main();
